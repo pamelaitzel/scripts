2881a2debed82746b7c876b007cf18395ab48306
pamelaitzel <135767172+pamelaitzel@users.noreply.github.com>
2024-05-18 19:31:41 -0600

    Create pigs.sh

---
+++ pigs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]; then
+  echo "Por favor, ejecuta como root"
+  exit
+fi
+read -p "Introduce la dirección IP para denegar los pings: " ip_address
+if [[ -z "$ip_address" ]]; then
+  echo "No se ha ingresado una dirección IP válida."
+  exit 1
+fi
+iptables -A INPUT -s $ip_address -p icmp --icmp-type echo-request -j DROP
+echo "Las solicitudes de ping desde la IP $ip_address ahora están bloqueadas."
